a115eb10418156c3e215003ffbe23adf519f8c1c
X. Niu <141608325+NieRNiu@users.noreply.github.com>
2025-07-21 01:01:13 +0200

    Update api.py

--- LLM&VLM Deployment/api.py
+++ LLM&VLM Deployment/api.py
@@ -1,6 +1,6 @@
-ARK_API_KEY="00ccf5fc-e880-4ade-a2b5-6859689ed7d5"
+ARK_API_KEY="**************************"
 LLM_MODEL="doubao-1-5-pro-32k-250115"
 
 # 获取网址：https://console.volcengine.com/speech/service/8?s=g 
-APP_ID="2942391141"
-APP_TOKEN="rYEhQtOFAb5xokmvpfBYdbr1Bw2XcRwI" 
+APP_ID="*************"
+APP_TOKEN="*************" 
